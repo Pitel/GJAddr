b85c83bc4f2977f6f6c375f332b3e6583ab9a8d8
Bc. Jan Kaláb <pitlicek@gmail.com>
2012-03-23 11:37:40 +0100

    OOP like a boss

---
+++ src/cz/vutbr/fit/gja/gjaddr/gui/GroupsPanel.java
@@ -0,0 +1,119 @@
+package cz.vutbr.fit.gja.gjaddr.gui;
+
+import cz.vutbr.fit.gja.gjaddr.persistancelayer.Database;
+import cz.vutbr.fit.gja.gjaddr.persistancelayer.Group;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+/**
+ * Panel with groups
+ *
+ * @author Bc. Jan Kaláb <xkalab00@stud.fit,vutbr.cz>
+ */
+class GroupsPanel extends JPanel implements ActionListener {
+	private final Database db = new Database();
+	private final DefaultListModel listModel = new DefaultListModel();
+
+	public GroupsPanel() {
+		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		JLabel label = new JLabel("Groups");
+		label.setAlignmentX(CENTER_ALIGNMENT);
+		add(label);
+		fillList();
+		JList list = new JList(listModel);
+		list.setSelectedIndex(0);
+		list.addListSelectionListener(new GroupSelectionListener());
+		JScrollPane listScrollPane = new JScrollPane(list);
+		add(listScrollPane);
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new BoxLayout(buttons, BoxLayout.LINE_AXIS));
+		JButton add = new JButton(new ImageIcon(getClass().getResource("/res/plus.png"), "+"));
+		add.setActionCommand("addGroup");
+		add.addActionListener(this);
+		buttons.add(add);
+		JButton remove = new JButton(new ImageIcon(getClass().getResource("/res/minus.png"), "-"));
+		remove.setActionCommand("removeGroup");
+		remove.addActionListener(this);
+		buttons.add(remove);
+		add(buttons);
+	}
+
+	/**
+	 * Fills list with groups from db
+	 */
+	private void fillList() {
+		listModel.clear();
+		listModel.addElement(new Group(-1, "All"));
+		for (Group g : db.getAllGroups()) {
+			listModel.addElement(g);
+		}
+	}
+
+	/**
+	 * Listener for actions (add/remove groups)
+	 *
+	 * @param e Action
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if ("addGroup".equals(e.getActionCommand())) {
+			addGroup();
+		} else if ("removeGroup".equals(e.getActionCommand())) {
+			removeGroup();
+		}
+	}
+
+	/**
+	 * Listener class for groups list selection
+	 */
+	private class GroupSelectionListener implements ListSelectionListener {
+		public void valueChanged(ListSelectionEvent e) {
+			if (!e.getValueIsAdjusting()) {	//React only on final choice
+				JList list = (JList) e.getSource();
+				Group[] groups = Arrays.copyOf(list.getSelectedValues(), list.getSelectedValues().length, Group[].class);
+				System.out.println("Groups: " + Arrays.toString(groups));
+			}
+		}
+	}
+
+	/**
+	 * Function for adding group
+	 */
+	private void addGroup() {
+		//System.out.println("addGroup");
+		String name = (String) JOptionPane.showInputDialog(
+			this,
+			"Group name:",
+			"Add group",
+			JOptionPane.QUESTION_MESSAGE,
+			new ImageIcon(getClass().getResource("/res/plus.png"), "+"),
+			null,
+			""
+		);
+		if (!name.isEmpty()) {
+			System.out.println(name);
+			List<Group> group = db.addNewGroup(name);
+			fillList();
+		}
+	}
+
+	/**
+	 * Function for removing group
+	 */
+	private void removeGroup() {
+		System.out.println("removeGroup");
+	}
+}
--- src/cz/vutbr/fit/gja/gjaddr/gui/MainWindow.java
+++ src/cz/vutbr/fit/gja/gjaddr/gui/MainWindow.java
@@ -9,29 +9,23 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.Arrays;
-import java.util.List;
 import javax.swing.BoxLayout;
-import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.KeyStroke;
-import javax.swing.ImageIcon;
 import javax.swing.UIManager;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import org.jdesktop.swingx.MultiSplitLayout.Divider;
 import org.jdesktop.swingx.MultiSplitLayout.Leaf;
 import org.jdesktop.swingx.MultiSplitLayout.Split;
@@ -47,7 +41,6 @@ public class MainWindow extends JFrame implements ActionListener, DocumentListen
 	private final Database db = new Database();
 	private JMenuItem menuItemClose, menuItemHelp, menuItemAbout, menuItemImport, menuItemExport;
 	private final JTextField searchField = new JTextField();
-	private final DefaultListModel groupsListModel = new DefaultListModel();
 
 	/**
 	 * Creates the main window.
@@ -80,7 +73,7 @@ public class MainWindow extends JFrame implements ActionListener, DocumentListen
 		model.setChildren(Arrays.asList(groupsLeaf, new Divider(), contactsLeaf, new Divider(), detailLeaf));
 		MultiSplitPane multiSplitPane = new MultiSplitPane();
 		multiSplitPane.getMultiSplitLayout().setModel(model);
-		multiSplitPane.add(groupPanel(), "groups");
+		multiSplitPane.add(new GroupsPanel(), "groups");
 		multiSplitPane.add(contactsPanel(), "contacts");
 		multiSplitPane.add(new JButton("Detail"), "detail");
 		container.add(multiSplitPane, BorderLayout.CENTER);
@@ -125,81 +118,6 @@ public class MainWindow extends JFrame implements ActionListener, DocumentListen
 		helpMenu.add(this.menuItemAbout);
 
 		return menuBar;
-	}
-
-	/**
-	 * Panel with groups
-	 *
-	 * @return Panel
-	 */
-	private JPanel groupPanel() {
-		JPanel panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-		JLabel label = new JLabel("Groups");
-		label.setAlignmentX(Component.CENTER_ALIGNMENT);
-		panel.add(label);
-		groupsListModel.addElement(new Group(-1, "All"));
-		for (Group g : db.getAllGroups()) {
-			groupsListModel.addElement(g);
-		}
-		JList list = new JList(groupsListModel);
-		list.setSelectedIndex(0);
-		list.addListSelectionListener(new GroupSelectionListener());
-		JScrollPane listScrollPane = new JScrollPane(list);
-		panel.add(listScrollPane);
-		JPanel buttons = new JPanel();
-		buttons.setLayout(new BoxLayout(buttons, BoxLayout.LINE_AXIS));
-		JButton add = new JButton(new ImageIcon(getClass().getResource("/res/plus.png"), "+"));
-		add.setActionCommand("addGroup");
-		add.addActionListener(this);
-		buttons.add(add);
-		JButton remove = new JButton(new ImageIcon(getClass().getResource("/res/minus.png"), "-"));
-		remove.setActionCommand("removeGroup");
-		remove.addActionListener(this);
-		buttons.add(remove);
-		panel.add(buttons);
-		return panel;
-	}
-
-	/**
-	 * Listener class for groups list
-	 */
-	private class GroupSelectionListener implements ListSelectionListener {
-		public void valueChanged(ListSelectionEvent e) {
-			if (!e.getValueIsAdjusting()) {	//React only on final choice
-				JList list = (JList) e.getSource();
-				Group[] groups = Arrays.copyOf(list.getSelectedValues(), list.getSelectedValues().length, Group[].class);
-				System.out.println("Groups: " + Arrays.toString(groups));
-			}
-		}
-	}
-
-	/**
-	 * Function for adding group
-	 */
-	private void addGroup() {
-		//System.out.println("addGroup");
-		String name = (String) JOptionPane.showInputDialog(
-			this,
-			"Group name:",
-			"Add group",
-			JOptionPane.QUESTION_MESSAGE,
-			new ImageIcon(getClass().getResource("/res/plus.png"), "+"),
-			null,
-			""
-		);
-		if (!name.isEmpty()) {
-			System.out.println(name);
-			List<Group> group = db.addNewGroup(name);
-			groupsListModel.addElement(group.get(0));
-		}
-	}
-
-	/**
-	 * Function for removing group
-	 */
-	private void removeGroup() {
-		System.out.println("removeGroup");
 	}
 
 	/**
@@ -269,10 +187,6 @@ public class MainWindow extends JFrame implements ActionListener, DocumentListen
 		//System.out.println(e.toString());
 		if (e.getSource() == this.menuItemClose) {
 			dispose();
-		} else if ("addGroup".equals(e.getActionCommand())) {
-			addGroup();
-		} else if ("removeGroup".equals(e.getActionCommand())) {
-			removeGroup();
 		}
 	}
 }
