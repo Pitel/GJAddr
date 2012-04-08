d9e9d5e67b09f3f22198e836e9b2faf46afbd058
Bc. Jan Kaláb <pitlicek@gmail.com>
2012-04-08 14:15:06 +0200

    Fixed formatting, again

--- src/cz/vutbr/fit/gja/gjaddr/gui/ContactsPanel.java
+++ src/cz/vutbr/fit/gja/gjaddr/gui/ContactsPanel.java
@@ -16,12 +16,8 @@ import javax.swing.table.TableRowSorter;
 class ContactsPanel extends JPanel {
 	static final long serialVersionUID = 0;
 	private static final Database db = new Database();
-	private static final BeanReaderJTable<Contact> table = 
-					new BeanReaderJTable<Contact>(new String[] {"FullName", "AllEmails", "AllPhones"}, 
-																				new String[] {"Name", "Emails", "Phones"});																									
-																				
-	private static final TableRowSorter<BeanReaderJTable.GenericTableModel> sorter = 
-					new TableRowSorter<BeanReaderJTable.GenericTableModel>(table.getModel());
+	private static final BeanReaderJTable<Contact> table = new BeanReaderJTable<Contact>(new String[] {"FullName", "AllEmails", "AllPhones"}, new String[] {"Name", "Emails", "Phones"});
+	private static final TableRowSorter<BeanReaderJTable.GenericTableModel> sorter = new TableRowSorter<BeanReaderJTable.GenericTableModel>(table.getModel());
 
 	/**
 	 * Constructor
