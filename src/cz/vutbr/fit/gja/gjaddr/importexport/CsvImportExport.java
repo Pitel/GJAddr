c05854facd271de5fe04fbb71483a57244ec8994
Drahomira Herrmannova <d.herrmannova@gmail.com>
2012-05-19 21:15:05 +0100

    Added messenger support to CSV import/export. #59

--- src/cz/vutbr/fit/gja/gjaddr/importexport/CsvImportExport.java
+++ src/cz/vutbr/fit/gja/gjaddr/importexport/CsvImportExport.java
@@ -3,6 +3,7 @@ package cz.vutbr.fit.gja.gjaddr.importexport;
 import au.com.bytecode.opencsv.CSVReader;
 import au.com.bytecode.opencsv.CSVWriter;
 import cz.vutbr.fit.gja.gjaddr.persistancelayer.*;
+import cz.vutbr.fit.gja.gjaddr.persistancelayer.util.MessengersEnum;
 import cz.vutbr.fit.gja.gjaddr.persistancelayer.util.TypesEnum;
 import java.io.*;
 import java.util.ArrayList;
@@ -17,150 +18,179 @@ import org.slf4j.LoggerFactory;
  */
 public class CsvImportExport {
 
-  /**
-   * Application database.
-   */
-  private Database database = Database.getInstance();
+    /**
+     * Application database.
+     */
+    private Database database = Database.getInstance();
 
-  /**
-   * Get group by it's name.
-   *
-   * @param groupName
-   * @return
-   */
-  private Group getGroupByName(String groupName) {
-    for (Group g : this.database.getAllGroups()) {
-      if (g.getName().equals(groupName)) {
-        return g;
-      }
-    }
-    return null;
-  }
-
-  /**
-   * Import contacts from CSV file.
-   *
-   * @param file
-   */
-  public int importContacts(File file) throws IOException {
-    return this.importContacts(file, null);
-  }
-
-  /**
-   * Import contacts from CSV file to specific group.
-   *
-   * @param file
-   * @param group
-   */
-  public int importContacts(File file, String group) throws IOException {
-    CSVReader reader = new CSVReader(new FileReader(file));
-    List<Contact> contacts = new ArrayList<Contact>();
-    String[] nextLine;
-
-    while ((nextLine = reader.readNext()) != null) {
-      // each line must have 7 parts
-      if (nextLine.length < 7) {
-        continue;
-      }
-
-      // create new contact
-      Contact contact = new Contact(nextLine[1], nextLine[0], nextLine[2], nextLine[3]);
-
-      // set contact emails
-      String[] emails = nextLine[4].split(",");
-      List<Email> emailsList = new ArrayList<Email>();
-      for (String email : emails) {
-        emailsList.add(new Email(TypesEnum.HOME, email));
-      }
-      contact.setEmails(emailsList);
-
-      // set contact phones
-      String[] phones = nextLine[5].split(",");
-      List<PhoneNumber> phonesList = new ArrayList<PhoneNumber>();
-      for (String phone : phones) {
-        phonesList.add(new PhoneNumber(TypesEnum.HOME, phone.trim()));
-      }
-      contact.setPhoneNumbers(phonesList);
-
-      // set contact addresses
-      String[] addresses = nextLine[6].split(";");
-      List<Address> addressesList = new ArrayList<Address>();
-      for (String address : addresses) {
-        addressesList.add(new Address(TypesEnum.HOME, address.replaceAll("(\\r|\\n)", ", ")));
-      }
-      contact.setAdresses(addressesList);
-
-      // TODO URLs
-
-      // add contact to list to be imported
-      contacts.add(contact);
-      LoggerFactory.getLogger(this.getClass()).debug("Adding contact ["
-              + nextLine[1] + " " + nextLine[0] + "].");
+    /**
+     * Get group by it's name.
+     *
+     * @param groupName
+     * @return
+     */
+    private Group getGroupByName(String groupName) {
+        for (Group g : this.database.getAllGroups()) {
+            if (g.getName().equals(groupName)) {
+                return g;
+            }
+        }
+        return null;
     }
 
-    // save contacts in database
-    if (group == null || group.isEmpty()) {
-      LoggerFactory.getLogger(this.getClass()).debug("Group is empty.");
-    } else {
-      LoggerFactory.getLogger(this.getClass()).debug("Adding contacts to : " + group);
+    /**
+     * Import contacts from CSV file.
+     *
+     * @param file
+     */
+    public int importContacts(File file) throws IOException {
+        return this.importContacts(file, null);
     }
 
-    // first add contacts to database
-    this.database.addNewContacts(contacts);
+    /**
+     * Import contacts from CSV file to specific group.
+     *
+     * @param file
+     * @param group
+     */
+    public int importContacts(File file, String group) throws IOException {
+        CSVReader reader = new CSVReader(new FileReader(file));
+        List<Contact> contacts = new ArrayList<Contact>();
+        String[] nextLine;
 
-    // then add to group
-    if (group != null) {
-      this.database.addNewGroup(group);
-      Group dbGroup = this.getGroupByName(group);
-      if (dbGroup != null) {
-        this.database.addContactsToGroup(dbGroup, contacts);
-      }
+        while ((nextLine = reader.readNext()) != null) {
+            // each line must have 7 parts
+            if (nextLine.length < 7) {
+                continue;
+            }
+
+            // create new contact
+            Contact contact = new Contact(nextLine[1], nextLine[0], nextLine[2], nextLine[3]);
+
+            // set contact emails
+            String[] emails = nextLine[4].split(",");
+            List<Email> emailsList = new ArrayList<Email>();
+            for (String email : emails) {
+                emailsList.add(new Email(TypesEnum.HOME, email));
+            }
+            contact.setEmails(emailsList);
+
+            // set contact phones
+            String[] phones = nextLine[5].split(",");
+            List<PhoneNumber> phonesList = new ArrayList<PhoneNumber>();
+            for (String phone : phones) {
+                phonesList.add(new PhoneNumber(TypesEnum.HOME, phone.trim()));
+            }
+            contact.setPhoneNumbers(phonesList);
+
+            // set contact addresses
+            String[] addresses = nextLine[6].split(";");
+            List<Address> addressesList = new ArrayList<Address>();
+            for (String address : addresses) {
+                addressesList.add(new Address(TypesEnum.HOME, address.replaceAll("(\\r|\\n)", ", ")));
+            }
+            contact.setAdresses(addressesList);
+            
+            // set contact messengers
+            String[] messengers = nextLine[7].split(",");
+            LoggerFactory.getLogger(this.getClass()).debug(String.valueOf(messengers.length));
+            List<Messenger> messengerList = new ArrayList<Messenger>();
+            for (String messenger : messengers) {
+                LoggerFactory.getLogger(this.getClass()).debug(messenger);
+                String[] m = messenger.split(":");
+                if (m.length < 2) {
+                    messengerList.add(new Messenger(MessengersEnum.OTHER, messenger.trim()));
+                } else {
+                    if (m[0].trim().equalsIgnoreCase(MessengersEnum.ICQ.toString())) {
+                        messengerList.add(new Messenger(MessengersEnum.ICQ, m[1].trim()));
+                    } else if (m[0].trim().equalsIgnoreCase(MessengersEnum.JABBER.toString())) {
+                        messengerList.add(new Messenger(MessengersEnum.JABBER, m[1].trim()));
+                    } else if (m[0].trim().equalsIgnoreCase(MessengersEnum.SKYPE.toString())) {
+                        messengerList.add(new Messenger(MessengersEnum.SKYPE, m[1].trim()));
+                    } else {
+                        messengerList.add(new Messenger(MessengersEnum.OTHER, m[1].trim()));
+                    }
+                }                
+            }
+            contact.setMessenger(messengerList);
+            
+            // set contact URLs
+            String[] urls = nextLine[8].split(",");
+            List<Url> urlList = new ArrayList<Url>();
+            for (String url : urls) {
+                urlList.add(new Url(TypesEnum.OTHER, url.trim()));
+            }
+            contact.setUrls(urlList);
+
+            // add contact to list to be imported
+            contacts.add(contact);
+            LoggerFactory.getLogger(this.getClass()).debug("Adding contact ["
+                    + nextLine[1] + " " + nextLine[0] + "].");
+        }
+
+        // save contacts in database
+        if (group == null || group.isEmpty()) {
+            LoggerFactory.getLogger(this.getClass()).debug("Group is empty.");
+        } else {
+            LoggerFactory.getLogger(this.getClass()).debug("Adding contacts to : " + group);
+        }
+
+        // first add contacts to database
+        this.database.addNewContacts(contacts);
+
+        // then add to group
+        if (group != null) {
+            this.database.addNewGroup(group);
+            Group dbGroup = this.getGroupByName(group);
+            if (dbGroup != null) {
+                this.database.addContactsToGroup(dbGroup, contacts);
+            }
+        }
+
+        return contacts.size();
     }
 
-    return contacts.size();
-  }
+    /**
+     * Export contacts to CSV file.
+     *
+     * @param file
+     * @param contacts
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public void exportContacts(File file, List<Contact> contacts)
+            throws FileNotFoundException, IOException {
 
-  /**
-   * Export contacts to CSV file.
-   *
-   * @param file
-   * @param contacts
-   * @throws FileNotFoundException
-   * @throws IOException
-   */
-  public void exportContacts(File file, List<Contact> contacts)
-          throws FileNotFoundException, IOException {
+        FileOutputStream fos = null;
+        OutputStreamWriter osw = null;
 
-    FileOutputStream fos = null;
-    OutputStreamWriter osw = null;
+        try {
+            fos = new FileOutputStream(file);
+            osw = new OutputStreamWriter(fos);
 
-    try {
-      fos = new FileOutputStream(file);
-      osw = new OutputStreamWriter(fos);
+            CSVWriter writer = new CSVWriter(new FileWriter(file));
 
-      CSVWriter writer = new CSVWriter(new FileWriter(file));
-      // TODO correct format
+            for (Contact c : contacts) {
+                List<String> entries = new ArrayList<String>();
+                entries.add(c.getSurName());
+                entries.add(c.getFirstName());
+                entries.add(c.getNickName());
+                entries.add(c.getNote());
+                entries.add(c.getAllEmails());
+                entries.add(c.getAllPhones());
+                entries.add(c.getAllAddresses());
+                entries.add(c.getAllMessengersWithType());
+                entries.add(c.getAllUrls());
+                writer.writeNext(entries.toArray(new String[entries.size()]));
+            }
 
-      for (Contact c : contacts) {
-        List<String> entries = new ArrayList<String>();
-        entries.add(c.getSurName());
-        entries.add(c.getFirstName());
-        entries.add(c.getNickName());
-        entries.add(c.getNote());
-        entries.add(c.getAllEmails());
-        entries.add(c.getAllPhones());
-        entries.add(c.getAllAddresses());
-        // TODO URLs
-        writer.writeNext(entries.toArray(new String[entries.size()]));
-      }
-
-      writer.close();
-    } finally {
-      try {
-        osw.close();
-      } finally {
-        fos.close();
-      }
+            writer.close();
+        } finally {
+            try {
+                osw.close();
+            } finally {
+                fos.close();
+            }
+        }
     }
-  }
 }
--- src/cz/vutbr/fit/gja/gjaddr/importexport/FacebookImport.java
+++ src/cz/vutbr/fit/gja/gjaddr/importexport/FacebookImport.java
@@ -27,236 +27,235 @@ import org.slf4j.LoggerFactory;
  *
  * @author Bc. Drahomira Herrmannova <xherrm01@stud.fit.vutbr.cz>
  * @see <a href="http://restfb.com">RestFB</a>
- * @see <a href="https://developers.facebook.com/docs/reference/api/user">Facebook Graph
- * API</a>
+ * @see <a href="https://developers.facebook.com/docs/reference/api/user">Facebook Graph API</a>
  */
 public class FacebookImport {
 
-  /**
-   * Local database.
-   */
-  private Database database;
-  /**
-   * Authentication token for Facebook.
-   */
-  private AuthToken token;
-  /**
-   * Facebook client for reading contacts.
-   */
-  private FacebookClient client;
-  /**
-   * How many contacts were imported so far.
-   */
-  private Integer processed = 0;
+    /**
+     * Local database.
+     */
+    private Database database;
+    /**
+     * Authentication token for Facebook.
+     */
+    private AuthToken token;
+    /**
+     * Facebook client for reading contacts.
+     */
+    private FacebookClient client;
+    /**
+     * How many contacts were imported so far.
+     */
+    private Integer processed = 0;
 
-  /**
-   * Constructor.
-   */
-  public FacebookImport() {
-    this.database = Database.getInstance();
-    this.token = this.database.getToken(ServicesEnum.FACEBOOK);
-    if (this.token != null) {
-      this.client = new DefaultFacebookClient(this.token.getToken());
-    } else {
-      this.client = null;
-    }
-  }
-
-  /**
-   * Get group by it's name.
-   *
-   * @param groupName
-   * @return
-   */
-  private Group getGroupByName(String groupName) {
-    for (Group g : this.database.getAllGroups()) {
-      if (g.getName().equals(groupName)) {
-        return g;
-      }
-    }
-    return null;
-  }
-
-  /**
-   * Fetch contact information from Facebook and save it in Contact class.
-   *
-   * @param id
-   * @return
-   */
-  private Contact fetchContact(String id) {
-    User user = this.client.fetchObject(id, User.class);
-
-    List<Email> emails = new ArrayList<Email>();
-    List<Url> urls = new ArrayList<Url>();
-    List<Custom> customFields = new ArrayList<Custom>();
-    List<Event> events = new ArrayList<Event>();
-
-    Contact contact = new Contact(user.getFirstName(), user.getLastName(), user.getUsername(), user.getAbout());
-
-    // contact emails
-    if (user.getEmail() != null && !user.getEmail().isEmpty() && !user.getEmail().equalsIgnoreCase("null")) {
-      emails.add(new Email(TypesEnum.HOME, user.getEmail()));
-      contact.setEmails(emails);
-    }
-
-    // contact URLs
-    if (user.getLink() != null && !user.getLink().isEmpty() && !user.getLink().equalsIgnoreCase("null")) {
-      urls.add(new Url(TypesEnum.HOME, user.getLink()));
-    }
-    if (user.getWebsite() != null && !user.getWebsite().isEmpty() && !user.getWebsite().equalsIgnoreCase("null")) {
-      urls.add(new Url(TypesEnum.HOME, user.getWebsite()));
-    }
-    if (!urls.isEmpty()) {
-      contact.setUrls(urls);
-    }
-
-    // set birthday
-    if (user.getBirthdayAsDate() != null) {
-      events.add(new Event(EventsEnum.BIRTHDAY, user.getBirthdayAsDate()));
-      contact.setDates(events);
-    }
-
-    // download photo
-    ImageIcon icon = null;
-    try {
-      URL iconUrl = new URL("http://graph.facebook.com/" + id + "/picture");
-      InputStream rd = iconUrl.openConnection().getInputStream();
-      ByteArrayOutputStream out = new ByteArrayOutputStream();
-      byte[] buffer = new byte[4096];
-      int read;
-      while (true) {
-        if ((read = rd.read(buffer)) != -1) {
-          out.write(buffer, 0, read);
+    /**
+     * Constructor.
+     */
+    public FacebookImport() {
+        this.database = Database.getInstance();
+        this.token = this.database.getToken(ServicesEnum.FACEBOOK);
+        if (this.token != null) {
+            this.client = new DefaultFacebookClient(this.token.getToken());
         } else {
-          break;
+            this.client = null;
         }
-      }
-      icon = new ImageIcon(out.toByteArray());
-    } catch (MalformedURLException ex) {
-      LoggerFactory.getLogger(this.getClass()).error(LoggerUtil.getStackTrace(ex));
-    } catch (IOException ex) {
-      LoggerFactory.getLogger(this.getClass()).error(LoggerUtil.getStackTrace(ex));
     }
 
-    // set photo
-    if (icon != null) {
-      contact.setPhoto(icon);
-    }
-
-    // custom fields
-    if (user.getHometownName() != null && !user.getHometownName().isEmpty()) {
-      customFields.add(new Custom("hometown", user.getHometownName()));
-    }
-    contact.setCustoms(customFields);
-
-    // address
-    List<Address> addresses = new ArrayList<Address>();
-    if (user.getLocation() != null && user.getLocation().getName() != null) {
-      addresses.add(new Address(TypesEnum.HOME, user.getLocation().getName()));
-      contact.setAdresses(addresses);
-    }
-
-    return contact;
-  }
-
-  /**
-   * Fetch user contacts.
-   *
-   * @return
-   */
-  private List<Contact> fetchContacts() throws FacebookImportException {
-    if (this.client == null) {
-      throw new FacebookImportException("You need to connect to Facebook before importing.\n"
-              + "Please go to Preferences and setup connection to Facebook.");
-    }
-
-    // fetch user friends
-    Connection<User> userFriends;
-    try {
-      userFriends = this.client.fetchConnection("me/friends", User.class);
-    } catch (FacebookOAuthException ex) {
-      throw new FacebookImportException("You need to connect to Facebook before importing.\n"
-              + "Please go to Preferences and setup connection to Facebook.");
-    }
-
-    // create list with contacts to import
-    List<Contact> contactsToImport = new ArrayList<Contact>();
-    // count how many contacts are there to import
-    int total = 0;
-    for (List<User> userList : userFriends) {
-      total += userList.size();
-    }
-    StatusBar.setProgressBounds(0, total);
-    StatusBar.setMessage("Importing contacts...");
-    boolean end = false;
-    // facebook returns the list of users in parts
-    for (List<User> userList : userFriends) {
-      for (User user : userList) {
-        if (FacebookImportThread.isThreadInterrupted()) {
-          end = true;
-          break;
+    /**
+     * Get group by it's name.
+     *
+     * @param groupName
+     * @return
+     */
+    private Group getGroupByName(String groupName) {
+        for (Group g : this.database.getAllGroups()) {
+            if (g.getName().equals(groupName)) {
+                return g;
+            }
         }
+        return null;
+    }
+
+    /**
+     * Fetch contact information from Facebook and save it in Contact class.
+     *
+     * @param id
+     * @return
+     */
+    private Contact fetchContact(String id) {
+        User user = this.client.fetchObject(id, User.class);
+
+        List<Email> emails = new ArrayList<Email>();
+        List<Url> urls = new ArrayList<Url>();
+        List<Custom> customFields = new ArrayList<Custom>();
+        List<Event> events = new ArrayList<Event>();
+
+        Contact contact = new Contact(user.getFirstName(), user.getLastName(), user.getUsername(), user.getAbout());
+
+        // contact emails
+        if (user.getEmail() != null && !user.getEmail().isEmpty() && !user.getEmail().equalsIgnoreCase("null")) {
+            emails.add(new Email(TypesEnum.HOME, user.getEmail()));
+            contact.setEmails(emails);
+        }
+
+        // contact URLs
+        if (user.getLink() != null && !user.getLink().isEmpty() && !user.getLink().equalsIgnoreCase("null")) {
+            urls.add(new Url(TypesEnum.HOME, user.getLink()));
+        }
+        if (user.getWebsite() != null && !user.getWebsite().isEmpty() && !user.getWebsite().equalsIgnoreCase("null")) {
+            urls.add(new Url(TypesEnum.HOME, user.getWebsite()));
+        }
+        if (!urls.isEmpty()) {
+            contact.setUrls(urls);
+        }
+
+        // set birthday
+        if (user.getBirthdayAsDate() != null) {
+            events.add(new Event(EventsEnum.BIRTHDAY, user.getBirthdayAsDate()));
+            contact.setDates(events);
+        }
+
+        // download photo
+        ImageIcon icon = null;
         try {
-          contactsToImport.add(this.fetchContact(user.getId()));
-          this.processed++;
-          StatusBar.setProgressValue(this.processed);
-        } catch (Exception e) {
-          LoggerFactory.getLogger(this.getClass()).error("Import of contact failed: {}", LoggerUtil.getStackTrace(e));
-          continue;
+            URL iconUrl = new URL("http://graph.facebook.com/" + id + "/picture");
+            InputStream rd = iconUrl.openConnection().getInputStream();
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] buffer = new byte[4096];
+            int read;
+            while (true) {
+                if ((read = rd.read(buffer)) != -1) {
+                    out.write(buffer, 0, read);
+                } else {
+                    break;
+                }
+            }
+            icon = new ImageIcon(out.toByteArray());
+        } catch (MalformedURLException ex) {
+            LoggerFactory.getLogger(this.getClass()).error(LoggerUtil.getStackTrace(ex));
+        } catch (IOException ex) {
+            LoggerFactory.getLogger(this.getClass()).error(LoggerUtil.getStackTrace(ex));
         }
-      }
-      if (end) {
-        break;
-      }
-    }
-    return contactsToImport;
-  }
 
-  /**
-   * Import contacts from Facebook to selected group.
-   *
-   * @param group
-   */
-  public int importContacts(String group) throws FacebookImportException {
-    // set progress
-    this.processed = 0;
-    // fetch contacts from facebook
-    List<Contact> contacts = this.fetchContacts();
+        // set photo
+        if (icon != null) {
+            contact.setPhoto(icon);
+        }
 
-    // save contacts in database
-    if (group == null || group.isEmpty()) {
-      LoggerFactory.getLogger(this.getClass()).debug("Group is empty.");
-    } else {
-      LoggerFactory.getLogger(this.getClass()).debug("Adding contacts to : " + group);
+        // custom fields
+        if (user.getHometownName() != null && !user.getHometownName().isEmpty()) {
+            customFields.add(new Custom("hometown", user.getHometownName()));
+        }
+        contact.setCustoms(customFields);
+
+        // address
+        List<Address> addresses = new ArrayList<Address>();
+        if (user.getLocation() != null && user.getLocation().getName() != null) {
+            addresses.add(new Address(TypesEnum.HOME, user.getLocation().getName()));
+            contact.setAdresses(addresses);
+        }
+
+        return contact;
     }
 
-    // first add contacts to database
-    this.database.addNewContacts(contacts);
+    /**
+     * Fetch user contacts.
+     *
+     * @return
+     */
+    private List<Contact> fetchContacts() throws FacebookImportException {
+        if (this.client == null) {
+            throw new FacebookImportException("You need to connect to Facebook before importing.\n"
+                    + "Please go to Preferences and setup connection to Facebook.");
+        }
 
-    // then add to group
-    if (group != null) {
-      this.database.addNewGroup(group);
-      Group dbGroup = this.getGroupByName(group);
-      if (dbGroup != null) {
-        this.database.addContactsToGroup(dbGroup, contacts);
-      }
+        // fetch user friends
+        Connection<User> userFriends;
+        try {
+            userFriends = this.client.fetchConnection("me/friends", User.class);
+        } catch (FacebookOAuthException ex) {
+            throw new FacebookImportException("You need to connect to Facebook before importing.\n"
+                    + "Please go to Preferences and setup connection to Facebook.");
+        }
+
+        // create list with contacts to import
+        List<Contact> contactsToImport = new ArrayList<Contact>();
+        // count how many contacts are there to import
+        int total = 0;
+        for (List<User> userList : userFriends) {
+            total += userList.size();
+        }
+        StatusBar.setProgressBounds(0, total);
+        StatusBar.setMessage("Importing contacts...");
+        boolean end = false;
+        // facebook returns the list of users in parts
+        for (List<User> userList : userFriends) {
+            for (User user : userList) {
+                if (FacebookImportThread.isThreadInterrupted()) {
+                    end = true;
+                    break;
+                }
+                try {
+                    contactsToImport.add(this.fetchContact(user.getId()));
+                    this.processed++;
+                    StatusBar.setProgressValue(this.processed);
+                } catch (Exception e) {
+                    LoggerFactory.getLogger(this.getClass()).error("Import of contact failed: {}", LoggerUtil.getStackTrace(e));
+                    continue;
+                }
+            }
+            if (end) {
+                break;
+            }
+        }
+        return contactsToImport;
     }
 
-    StatusBar.setProgressFinished();
+    /**
+     * Import contacts from Facebook to selected group.
+     *
+     * @param group
+     */
+    public int importContacts(String group) throws FacebookImportException {
+        // set progress
+        this.processed = 0;
+        // fetch contacts from facebook
+        List<Contact> contacts = this.fetchContacts();
 
-    return contacts.size();
-  }
+        // save contacts in database
+        if (group == null || group.isEmpty()) {
+            LoggerFactory.getLogger(this.getClass()).debug("Group is empty.");
+        } else {
+            LoggerFactory.getLogger(this.getClass()).debug("Adding contacts to : " + group);
+        }
 
-  /**
-   * Test the class.
-   *
-   * @param args
-   */
-  public static void main(String[] args) throws FacebookImportException {
-    FacebookImport fb = new FacebookImport();
-    List<Contact> cs = fb.fetchContacts();
-    for (Contact c : cs) {
-      System.out.println(c.getFullName() + " " + c.getAllEmails());
+        // first add contacts to database
+        this.database.addNewContacts(contacts);
+
+        // then add to group
+        if (group != null) {
+            this.database.addNewGroup(group);
+            Group dbGroup = this.getGroupByName(group);
+            if (dbGroup != null) {
+                this.database.addContactsToGroup(dbGroup, contacts);
+            }
+        }
+
+        StatusBar.setProgressFinished();
+
+        return contacts.size();
     }
-  }
+
+    /**
+     * Test the class.
+     *
+     * @param args
+     */
+    public static void main(String[] args) throws FacebookImportException {
+        FacebookImport fb = new FacebookImport();
+        List<Contact> cs = fb.fetchContacts();
+        for (Contact c : cs) {
+            System.out.println(c.getFullName() + " " + c.getAllEmails());
+        }
+    }
 }
