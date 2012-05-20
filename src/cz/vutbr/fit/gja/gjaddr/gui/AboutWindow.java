209688200c93ff48230ea6b65fda88836f661dcf
Bc. Jan Kaláb <pitlicek@gmail.com>
2012-05-20 18:40:38 +0200

    Better About window #65

--- src/cz/vutbr/fit/gja/gjaddr/gui/AboutWindow.java
+++ src/cz/vutbr/fit/gja/gjaddr/gui/AboutWindow.java
@@ -15,37 +15,40 @@ class AboutWindow extends JDialog {
 
     public AboutWindow() {
         super();
-        this.setModal(true);
+        setModal(true);
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
             System.err.println(e);
         }
-        ImageIcon icon = new ImageIcon(getClass().getResource("/res/icon.png"), "GJAddr");
+        final ImageIcon icon = new ImageIcon(getClass().getResource("/res/icon.png"), "GJAddr");
         setIconImage(icon.getImage());
+        setTitle("About GJAddr");
         setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
-        JLabel header = new JLabel("<html><h1>GJAddr</h1></html>", JLabel.CENTER);
+        final JLabel header = new JLabel("<html><h1>GJAddr 1.0</h1></html>", JLabel.CENTER);
         header.setAlignmentX(CENTER_ALIGNMENT);
         add(header);
-        JLabel logo = new JLabel(icon, JLabel.CENTER);
+        final JLabel logo = new JLabel(icon, JLabel.CENTER);
         logo.setAlignmentX(CENTER_ALIGNMENT);
         add(logo);
-        JLabel authors = new JLabel("<html><h2>Authors</h2><ul><li>Bc. Radek Gajdušek</li><li>Bc. Drahomíra Herrmannová</li><li>Bc. Jan Kaláb</li></ul></html>", JLabel.CENTER);
-        authors.setAlignmentX(CENTER_ALIGNMENT);
-        add(authors);
+        final JLabel details = new JLabel("<html><center>Projekt do předmětu Grafická uživatelská rozhraní v Javě<br>FIT VUTBR 2012</center><h2>Authors</h2><ul><li>Bc. Radek Gajdušek</li><li>Bc. Drahomíra Herrmannová</li><li>Bc. Jan Kaláb</li></ul></html>", JLabel.CENTER);
+        details.setAlignmentX(CENTER_ALIGNMENT);
+        details.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        add(details);
         setResizable(false);
         setLocationRelativeTo(null);
-    
+
         pack();
         setVisible(true);
         LoggerFactory.getLogger(this.getClass()).info("Opening about window.");
     }
-    
+
     @Override
     protected JRootPane createRootPane() {
         JRootPane rp = new JRootPane();
         KeyStroke stroke = KeyStroke.getKeyStroke("ESCAPE");
         Action actionListener = new AbstractAction() {
+            static final long serialVersionUID = 0;
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 dispose();
